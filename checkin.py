37b87223d134936fb754efa6865639c6927f211d
zhangycy <58470922+zhangycy@users.noreply.github.com>
2021-03-18 23:29:48 +0800

    Update checkin.py

--- checkin.py
+++ checkin.py
@@ -28,6 +28,7 @@ def start():
 
     if 'message' in checkin.text:
         mess = checkin.json()['message']
+        print(state.json())
         time = state.json()['data']['leftDays']
         time = time.split('.')[0]
         #print(time)
